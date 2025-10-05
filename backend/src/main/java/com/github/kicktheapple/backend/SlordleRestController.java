48e28f72077cca0631610304b29e5a6cc33bf70a
Domenic Dvojmoc <applecakes811@gmail.com>
2025-10-05 02:46:16 +0200

    Finished the Backend with working api calls

--- backend/src/main/java/com/github/kicktheapple/backend/SlordleRestController.java
+++ backend/src/main/java/com/github/kicktheapple/backend/SlordleRestController.java
@@ -7,22 +7,46 @@ public class SlordleRestController {
 
     public String theWorld = "BAKER";
 
+    enum Colors {
+        WHITE,
+        GREEN,
+        YELLOW,
+        GRAY
+    }
+
     @GetMapping("/api/test")
     public String getString() {
         return "Here's a string";
     }
 
     @PostMapping("/api/GuessTest")
-    public @ResponseBody Guess requestHandling(@RequestBody Guess body) {
-        return body;
+    public @ResponseBody Coloring requestHandling(@RequestBody Guess body) {
+        Coloring colors = new Coloring(body.word.length());
+        for (int i = 0; i < body.word.length(); i++) {
+            if (hardGuess(body.word.charAt(i), i)) {
+                colors.colors[i] = Colors.GREEN.ordinal();
+            } else {
+                if (softGuess(body.word.charAt(i), body.word)) {
+                    colors.colors[i] = Colors.YELLOW.ordinal();
+                } else {
+                    colors.colors[i] = Colors.GRAY.ordinal();
+                }
+            }
+        }
+        return colors;
     }
 
-    public boolean softGuess() {
+    public boolean softGuess(char letter, String guess) {
+        for (int i = 0; i < guess.length(); i++) {
+            if (letter == theWorld.charAt(i) && theWorld.charAt(i) != guess.charAt(i)) {
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean hardGuess() {
-        return false;
+    public boolean hardGuess(char letter, int position) {
+        return letter == theWorld.charAt(position);
     }
 
 }
